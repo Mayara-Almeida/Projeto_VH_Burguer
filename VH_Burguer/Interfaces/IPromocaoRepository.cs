2974a74d390c3ba6ce88f7c0141eddf4d2e4f434
Mayara <mayara.s.almeida8@aluno.senai.br>
2026-02-24 10:45:29 -0300

    :books: Iniciando CRUD de promoção

---
+++ VH_Burguer/Interfaces/IPromocaoRepository.cs
@@ -0,0 +1,15 @@
+﻿using VH_Burguer.Domains;
+
+namespace VH_Burguer.Interfaces
+{
+    public interface IPromocaoRepository
+    {
+        List<Promocao> Listar();
+        Promocao ObterPorId(int id);
+
+        bool NomeExiste(string nome, int? promocaoAtual = null);
+        void Adicionar(Promocao promocao);
+        void Atualizar(Promocao promocao);
+        void Remover(int id);
+    }
+}
--- VH_Burguer/obj/Debug/net8.0/VH_Burguer.AssemblyInfo.cs
+++ VH_Burguer/obj/Debug/net8.0/VH_Burguer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VH_Burguer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f7ddc9a31f3b6a566e7001019b83586955d2a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+706cfc56105c5d94eee244b1b9197e1bdd945aec")]
 [assembly: System.Reflection.AssemblyProductAttribute("VH_Burguer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VH_Burguer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
